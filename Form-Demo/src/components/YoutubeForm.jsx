3149679dcfa9e296ccae88889ec5ea45b737f5a5
fachry99 <151fachry@gmail.com>
2023-10-08 11:03:39 +0700

    basic form, add some css in app.css

---
+++ Form-Demo/src/components/YoutubeForm.jsx
@@ -0,0 +1,20 @@
+function YoutubeForm() {
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+
+        <label htmlFor="email">E-mail</label>
+        <input type="email" name="email" id="email" />
+
+        <label htmlFor="channel">Channel</label>
+        <input type="text" name="channel" id="channel" />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default YoutubeForm;
