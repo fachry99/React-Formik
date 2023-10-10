33f6a9eee17d00cfa4d9453fd389c0788aa01ec0
fachry99 <151fachry@gmail.com>
2023-10-10 09:45:42 +0700

    Field Revisited

--- Form-Demo/src/components/YoutubeForm.jsx
+++ Form-Demo/src/components/YoutubeForm.jsx
@@ -5,6 +5,8 @@ const initialValues = {
   name: "",
   email: "",
   channel: "",
+  comments: "",
+  address: "",
 };
 
 const onSubmit = (values) => {
@@ -38,8 +40,34 @@ function YoutubeForm() {
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <Field type="text" name="channel" id="channel" />
+          <Field
+            type="text"
+            name="channel"
+            id="channel"
+            placeholder="youtube chanel name"
+          />
           <ErrorMessage name="channel" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" name="comments" id="comments" />
+          <ErrorMessage name="comments" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {(props) => {
+              const { field, form, meta } = props;
+              return (
+                <div>
+                  <input type="text" id="address" {...field} />
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                </div>
+              );
+            }}
+          </Field>
         </div>
 
         <button type="submit">Submit</button>
