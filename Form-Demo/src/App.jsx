c4b2ffd82ff78db0867e1a2aac9e0660f7bfb3a3
fachry99 <151fachry@gmail.com>
2023-10-15 19:53:35 +0700

    Create Course Enrollment Form

--- Form-Demo/src/App.jsx
+++ Form-Demo/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import EnrollmentForm from "./components/EnrollmentForm";
 // import FormikContainer from "./components/FormikContainer";
 import LoginForm from "./components/LoginForm";
 import RegistrationForm from "./components/RegistrationForm";
@@ -7,7 +8,8 @@ import RegistrationForm from "./components/RegistrationForm";
 function App() {
   return (
     <div className="App">
-      <RegistrationForm />
+      <EnrollmentForm />
+      {/* <RegistrationForm /> */}
       {/* <LoginForm /> */}
       {/* <FormikContainer /> */}
       {/* <YoutubeForm /> */}
