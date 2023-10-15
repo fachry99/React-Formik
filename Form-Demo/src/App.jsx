5dd21a5b3e1a11ee911dc6fdd816363a39c557eb
fachry99 <151fachry@gmail.com>
2023-10-15 14:33:01 +0700

    create registrion form

--- Form-Demo/src/App.jsx
+++ Form-Demo/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 // import FormikContainer from "./components/FormikContainer";
 import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
 // import YoutubeForm from "./components/YoutubeForm";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      <RegistrationForm />
+      {/* <LoginForm /> */}
       {/* <FormikContainer /> */}
       {/* <YoutubeForm /> */}
     </div>
